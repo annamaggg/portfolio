fb5fb97d2ea3d866f7c76c5dd46778be743f699a
annamaggg <annamagnusson99@gmail.com>
2025-03-23 19:43:42 +0000

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+export const Navbar = () => {
+  return (
+    <div className='outer'>
+      <nav style={{textAlign: 'center'}}>
+        <ul className='list'>
+          <li><a href="#" className='pages'>Home</a></li>
+          <li><a href="#/software" className='pages'>Software</a></li>
+          <li><a href="#/gallery" className='pages'>Gallery</a></li>
+          <li><a href="#/about" className='pages'>About</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
